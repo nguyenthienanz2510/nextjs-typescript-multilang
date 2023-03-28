2dd45474e37b10501af3b00f9f47b13672c3b713
NguyenThienAn <nguyenthienan@mobcec.com>
2023-03-28 17:06:59 +0700

    Fix: sidebar

--- src/components/layouts/components/MainHeader/components/Sidebar.tsx
+++ src/components/layouts/components/MainHeader/components/Sidebar.tsx
@@ -7,7 +7,8 @@ import styles from './sidebar.module.scss'
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState<boolean>(false)
-  const sideBar = useRef<HTMLDivElement | null>(null)
+  const sideBarBody = useRef<HTMLDivElement | null>(null)
+  const overlayElement = useRef<HTMLDivElement | null>(null)
   const { t } = useTranslation(['common'])
   const { route } = useRouter()
   const dataNavbar = useMemo(
@@ -28,49 +29,54 @@ export default function Sidebar() {
     [t]
   )
 
-  const handleOpenSidebar = () => {
-    setIsOpen(!isOpen)
-    console.log(sideBar.current?.style)
-    document.querySelectorAll<HTMLElement>('.sidebar__body ul li').forEach((item, index) => {
-      if (item.style.animation.includes('sidebarItemAnimationShow')) {
-        item.style.animation = `${styles.sidebarItemAnimationHide} ease forwards ${1 / index + 0.25}s`
+  const handleSidebarSwitcher = () => {
+    if (!isOpen) {
+      setIsOpen(!isOpen)
+      overlayElement.current && (overlayElement.current.style.display = 'block')
+    } else {
+      setIsOpen(!isOpen)
+      overlayElement.current && (overlayElement.current.style.display = 'none')
+    }
+    sideBarBody.current?.children[0].childNodes.forEach((item, index) => {
+      if ((item as HTMLElement).style.animation.includes('sidebarAnimationShow')) {
+        ;(item as HTMLElement).style.animation = `${styles.sidebarAnimationHide} ease forwards ${1 / index + 0.25}s`
       } else {
-        item.style.animation = `${styles.sidebarItemAnimationShow} 0.5s ease forwards ${index / 10 + 0.25}s`
+        ;(item as HTMLElement).style.animation = `${styles.sidebarAnimationShow} 0.5s ease forwards ${
+          index / 10 + 0.25
+        }s`
       }
     })
   }
   return (
     <React.Fragment>
-      <div className={`${styles.sidebar} ${isOpen ? styles.active : ''} `} ref={sideBar}>
-        <div className={`${styles.sidebar__top} flex h-20 items-center justify-end pr-4 sm:pr-10`}>
-          <button className='ml-auto flex h-7 w-7 items-center justify-center'>
-            <label htmlFor='hamburgerIcon' className={styles['hamburger__icon']}>
-              <input id='hamburgerIcon' type='checkbox' onClick={handleOpenSidebar} />
-              <span />
-              <span />
-              <span />
-            </label>
-          </button>
-        </div>
-        <div className={`${styles.sidebar__body} sidebar__body px-10 py-5`}>
-          <ul>
-            {dataNavbar.map((item) => (
-              <li key={item.slug} className='py-5 opacity-0'>
-                <Link href={item.slug}>
-                  <span
-                    className={classNames('nav-link-hover-effect mr-12 text-12 uppercase', {
-                      'nav-link-hover-effect--active': item.slug == route
-                    })}
-                  >
-                    {item.name}
-                  </span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className={`${styles.sidebar__top} pr-4 sm:pr-10`}>
+        <button className='ml-auto flex h-6 w-7 items-center justify-center'>
+          <label htmlFor='hamburgerIcon' className={styles['hamburger__icon']}>
+            <input id='hamburgerIcon' type='checkbox' onClick={handleSidebarSwitcher} />
+            <span />
+            <span />
+            <span />
+          </label>
+        </button>
       </div>
-      <div className={`${styles.overlay} ${isOpen ? styles.active : ''}`}></div>
+      <div className={`${styles.sidebar__body} ${isOpen ? styles.active : ''} w-full sm:w-[420px]`} ref={sideBarBody}>
+        <ul>
+          {dataNavbar.map((item) => (
+            <li key={item.slug} className='py-5 opacity-0'>
+              <Link href={item.slug}>
+                <span
+                  className={classNames('nav-link-hover-effect text-14 uppercase', {
+                    'nav-link-hover-effect--active': item.slug == route
+                  })}
+                >
+                  {item.name}
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.overlay} ref={overlayElement}></div>
     </React.Fragment>
   )
 }
